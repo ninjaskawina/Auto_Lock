6cd00ca4aedeb9902bf30bd5f8a4631a3c199ded
ninjaskawina <ninjaskawina@gmail.com>
2016-05-17 09:49:20 +0200

    Update main.cpp
    Initial main loop and lock function and global variables

--- main.cpp
+++ main.cpp
@@ -1,15 +1,31 @@
 #include "hFramework.h"
 #include <stdio.h>
+#include <string>
 
 using namespace hFramework;
 
+    int power = 500;
+    int angle1 = -50;
+    int angle2 = 90;
+    bool block = 1;
+    bool locked = false;
+
+
+void lock_toggle(){
+    if(locked){
+            //Open
+    LED1.on();
+    } else{
+    LED1.off();
+    }
+    locked = !locked;
+}
+
 void hMain(void)
 {
-	sys.setLogDev(&Serial);
-	for (;;)
-	{
-		LED1.toggle();
-		printf("test %ld\r\n", sys.getRefTime());
-		sys.delay_ms(500);
-	}
+
+	hExt1.serial.printf("%c11090A41%c", (char)0x02, (char)0x03);
+	sys.delay_ms(100);
+
 }
+
